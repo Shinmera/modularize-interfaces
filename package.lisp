3ada4c87a364e90fac5da79de210ca611569a13f
Shinmera <shinmera@tymoon.eu>
2014-07-24 21:54:09 +0200

    Added exports

--- package.lisp
+++ package.lisp
@@ -7,6 +7,28 @@
 (defpackage org.tymoonnext.radiance.lib.modularize.interfaces
   (:use #:cl #:modularize #:trivial-indent)
   (:nicknames #:modularize-interfaces #:interfaces)
-  ;; 
+  ;; component.lisp
   (:export
-   ))
+   #:expand-component
+   #:define-component-expander
+   #:component-alias)
+  ;; interface.lisp
+  (:export
+   #:interface-not-found
+   #:interface-already-implemented
+   
+   #:interface
+   #:interface-p
+   #:implementation
+   #:reset-interface
+   #:define-interface
+   #:define-interface-extension
+   #:defimpl
+   #:define-interface-function
+   #:define-interface-macro
+   #:define-interface-method)
+  ;; toolkit.lisp
+  (:export
+   #:defun*
+   #:defmacro*
+   #:defmethod*))
